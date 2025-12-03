7b58713f4491e0ef6af0e8e48c0ddd42e10b2810
Harivansh <jazzz1815@gmail.com>
2025-12-03 23:42:41 +0530

    deploy done

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://coderower-software.onrender.com",
     methods: ["GET", "POST", "PUT"],
   })
 );
